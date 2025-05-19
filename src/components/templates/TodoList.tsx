54dd7103ab71ea5e015af70629c642b8c94a2854
shokixxx <uver_discord_0703@yahoo.co.jp>
2025-05-19 23:25:20 +0900

    TODOアプリとして初期修正およびディレクトリ構成の修正

---
+++ src/components/templates/TodoList.tsx
@@ -0,0 +1,7 @@
+export const TodoList = () => {
+  return (
+    <>
+      <h1>TODOList</h1>
+    </>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
+import './styles/index.css'
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+
+import { TodoList } from './components/templates/TodoList'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <TodoList />
   </StrictMode>
 )
