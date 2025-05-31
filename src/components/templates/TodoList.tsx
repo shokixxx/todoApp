2278d67debb72fcc7faef92f30fa36ca070fd1ef
shokixxx <uver_discord_0703@yahoo.co.jp>
2025-05-31 21:06:19 +0900

    TODOListに基本タグの追加とフォームの追加

--- src/components/templates/TodoList.tsx
+++ src/components/templates/TodoList.tsx
@@ -1,7 +1,33 @@
+import { useRef } from 'react'
+
+import { Button } from '@mui/material'
+
+import { InputField } from '../atmos/InputField'
+
 export const TodoList = () => {
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+    if (inputRef.current) {
+      const newTodo = inputRef.current.value
+      if (newTodo !== '') {
+        inputRef.current.value = ''
+      }
+    }
+  }
+
   return (
     <>
-      <h1>TODOList</h1>
+      <header>
+        <h1>TODOList</h1>
+      </header>
+      <main>
+        <form onSubmit={handleSubmit}>
+          <InputField ref={inputRef} />
+          <Button variant="contained">登録する</Button>
+        </form>
+      </main>
     </>
   )
 }
