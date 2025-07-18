f44c5dd8e5ab8916036485f12bf7c6fda3ccf910
shokixxx <uver_discord_0703@yahoo.co.jp>
2025-07-18 09:50:07 +0900

    登録されたtodoを管理し、listで表示する

--- src/components/templates/TodoList.tsx
+++ src/components/templates/TodoList.tsx
@@ -1,15 +1,25 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 
 import { AddTodoForm } from '../molecules/AddTodoForm'
 
+type TodoItem = {
+  id: number
+  name: string
+}
+
+// TODO: ここで管理せずUUIDなどを採番して管理すること
+let nextId = 0
+
 export const TodoList = () => {
   const inputRef = useRef<HTMLInputElement>(null)
+  const [todoItems, setTodoItems] = useState<TodoItem[]>([])
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault()
     if (inputRef.current) {
-      const newTodo = inputRef.current.value
-      if (newTodo !== '') {
+      const newTodoName = inputRef.current.value
+      if (newTodoName !== '') {
+        setTodoItems([...todoItems, { id: ++nextId, name: newTodoName }])
         inputRef.current.value = ''
       }
     }
@@ -24,6 +34,11 @@ export const TodoList = () => {
         <form onSubmit={handleSubmit}>
           <AddTodoForm inputRef={inputRef} />
         </form>
+        <ul>
+          {todoItems.map((todoItem) => (
+            <li key={todoItem.id}>{todoItem.name}</li>
+          ))}
+        </ul>
       </main>
     </>
   )
