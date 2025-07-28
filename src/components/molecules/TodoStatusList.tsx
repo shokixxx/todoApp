7d34a87e29edca1a03ea8528df254e3d5ec8185d
shokixxx <uver_discord_0703@yahoo.co.jp>
2025-07-28 23:11:50 +0900

    TodoStatusListのh2タグのレイアウトを修正

--- src/components/molecules/TodoStatusList.tsx
+++ src/components/molecules/TodoStatusList.tsx
@@ -7,7 +7,7 @@ type TodoStatusListProps = {
 export const TodoStatusList = ({ statusTitle, todoItems }: TodoStatusListProps) => {
   return (
     <>
-      <h2 className="m-5 text-2xl">{statusTitle}</h2>
+      <h2 className="m-5 text-center text-2xl font-bold">{statusTitle}</h2>
       <ul>
         {todoItems.map((todoItem) => (
           <li className="m-5" key={todoItem.id}>
