39490f591f25da6b66d1c12f8731a9f589967ee5
shokixxx <uver_discord_0703@yahoo.co.jp>
2025-07-31 00:45:58 +0900

    進行中ステータスにドロップされた時の処理を追加
     - ステータスを進行中に変えてliタグで表示する

--- src/components/templates/TodoList.tsx
+++ src/components/templates/TodoList.tsx
@@ -39,7 +39,18 @@ export const TodoList = () => {
               todoItems={todoItems.filter((todoItem) => todoItem.status === 'notStarted')}
             />
           </div>
-          <div className="w-1/3 min-w-[200px] rounded bg-pink-400 p-2">
+          <div
+            className="w-1/3 min-w-[200px] rounded bg-pink-400 p-2"
+            onDragOver={(event) => event.preventDefault()}
+            onDrop={(event) => {
+              const dropItemId = event.dataTransfer.getData('text/plain')
+              setTodoItems(
+                todoItems.map((todoItem) =>
+                  todoItem.id === Number(dropItemId) ? { ...todoItem, status: 'doing' } : todoItem
+                )
+              )
+            }}
+          >
             <TodoStatusList
               statusTitle="進行中"
               todoItems={todoItems.filter((todoItem) => todoItem.status === 'doing')}
