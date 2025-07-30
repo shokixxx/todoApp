092b1faaf52a814ff760356d61c49b3eabcc5749
shokixxx <uver_discord_0703@yahoo.co.jp>
2025-07-31 01:18:26 +0900

    todoの横に削除ボタンを追加

--- src/components/molecules/TodoStatusList.tsx
+++ src/components/molecules/TodoStatusList.tsx
@@ -1,3 +1,6 @@
+import DeleteIcon from '@mui/icons-material/Delete'
+import IconButton from '@mui/material/IconButton'
+
 import type { TodoItem } from '../../type/TodoItem'
 
 type TodoStatusListProps = {
@@ -12,11 +15,14 @@ export const TodoStatusList = ({ statusTitle, todoItems }: TodoStatusListProps) 
         {todoItems.map((todoItem) => (
           <li
             key={todoItem.id}
-            className="m-5 cursor-grab rounded border bg-gray-600 p-4 hover:bg-gray-700"
+            className="m-5 flex cursor-grab items-center justify-between rounded border bg-gray-600 p-4 hover:bg-gray-700"
             draggable
             onDragStart={(event) => event.dataTransfer.setData('text/plain', `${todoItem.id}`)}
           >
             {todoItem.name}
+            <IconButton aria-label="delete">
+              <DeleteIcon />
+            </IconButton>
           </li>
         ))}
       </ul>
